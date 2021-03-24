5aaf46c09d75b58506c11a598587ed6388257bea
solom <solom@192.168.10.71>
2021-03-24 12:53:25 +0900

    2021-03-24 12:53 롬복사용 sampleVO생성

---
+++ demo/src/main/java/egovframework/example/sample/vo/sampleVO.java
@@ -0,0 +1,12 @@
+package egovframework.example.sample.vo;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class sampleVO {
+	private String text;
+}
